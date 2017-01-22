eeaad8aa42145a6970a58d67c06556d57d937d06
Joshua Li <jrl5696@psu.edu>
2017-01-22 10:41:08 -0500

    add handle rate limits, connection errors and automate paginated calls

--- ghminer.py
+++ ghminer.py
@@ -1,3 +1,4 @@
+from time import time, sleep
 import requests
 
 # The metadata keys we're interested in. See get_user().
@@ -12,12 +13,20 @@ with open('oauthtk.txt', 'r') as f:
 def _req(url):
     '''Request wrapper. Returns dict representation of JSON response.
     Blocks and retries if request is not 200 or has hit the rate limit.'''
-    r = requests.get(url, headers={'Authorization':'token ' + __OAUTHTK})
+    try:
+        r = requests.get(url, headers={'Authorization':'token ' + __OAUTHTK})
+    except requests.ConnectionError:
+        sleep(1)
+        return _req(url)
     if r.status_code == 200:
-        # TODO handle rate limit
         return r.json()
+    elif r.status_code == 403:
+        wait_amt = int(r.headers['X-RateLimit-Reset']) - time()
+        sleep(int(wait_amt + 3))
+        return _req(url)
     else:
-        pass # TODO handle connectivity issues
+        raise Exception('Strange request: {} [{}]'.format(
+            r.url, r.status_code))
 
 def get_usernames(since_id):
     '''Returns a list of 100 usernames starting from a since id.'''
@@ -32,8 +41,14 @@ def get_user(username):
 
 
 def main():
-    for u in get_usernames(0):
-        print(get_user(u))
+    start_id = 0  # TODO pick off from last id in case of crash
+    while start_id < 200:  # temporary limit
+        usernames = get_usernames(start_id)
+        for i, name in enumerate(usernames):
+            u = get_user(name)
+            # TODO write to database
+            if i == len(usernames) - 1:
+                start_id = u['id']
 
 if __name__ == '__main__':
     main()
