c98454790b6ffbe450eade1b53ca97dbb3a72750
Joshua Li <jrl5696@psu.edu>
2017-01-22 14:12:18 -0500

    adding some cute print messages

--- ghminer.py
+++ ghminer.py
@@ -17,33 +17,33 @@ def _req(url):
     try:
         r = requests.get(url, headers={'Authorization':'token ' + __OAUTHTK})
     except requests.ConnectionError:
-        sleep(1)
+        print('[-] Internet connection error, retrying in 3...')
+        sleep(3)
         return _req(url)
     if r.status_code == 200:
         return r.json()
     elif r.status_code == 403:
-        wait_amt = int(r.headers['X-RateLimit-Reset']) - time()
-        sleep(int(wait_amt + 3))
+        wait_amt = int(int(r.headers['X-RateLimit-Reset']) - time()) + 3
+        print('[-] Rate limit exceeded, sleeping for {} seconds ...'.format(wait_amt))
+        sleep(wait_amt)
         return _req(url)
     else:
-        raise Exception('Strange request: {} [{}]'.format(
-            r.url, r.status_code))
+        raise Exception('FATAL: (strange request) {} [{}]'.format(r.url, r.status_code))
 
 def get_usernames(since_id):
     '''Returns a list of 100 usernames starting from a since id.'''
-    data = _req('https://api.github.com/users?per_page=100&since=' +
-        str(since_id))
+    data = _req('https://api.github.com/users?per_page=100&since=' + str(since_id))
     return [user.get('login', '') for user in data]
 
 def get_user(username):
     '''Returns a dict containing an account's desired metadata.'''
     data = _req('https://api.github.com/users/' + username)
-    return {k: data[k] for k in __KEEP}
+    return {k: data.get(k, None) for k in __KEEP}
 
 
 def main():
-    conn = sqlite3.connect('ghaccounts.sqlite3',
-        detect_types=sqlite3.PARSE_DECLTYPES)
+    print('Connecting to database')
+    conn = sqlite3.connect('ghaccounts.sqlite3', detect_types=sqlite3.PARSE_DECLTYPES)
 
     conn.execute('''
         CREATE TABLE IF NOT EXISTS accounts(
@@ -66,6 +66,7 @@ def main():
     cursor = conn.execute('SELECT max(id) FROM accounts')
     start_id = cursor.fetchone()[0]
     while start_id < 25*10**6:
+        print('Retrieving usernames after id #{}'.format(start_id))
         usernames = get_usernames(start_id)
         for i, name in enumerate(usernames):
             u = get_user(name)
@@ -73,7 +74,7 @@ def main():
                 ', '.join(__KEEP), ', '.join(['?']*len(__KEEP))),
                 [u[k] for k in __KEEP])
             conn.commit()
-            print('done uid', u['id'])
+            print('[+] metadata saved for account id #{}'.format(u['id']))
             if i == len(usernames) - 1:
                 start_id = u['id']
 
